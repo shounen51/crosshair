bf55418e4fe7dcc68fa0d2959a5d1bb700b7bad6
shounen51 <shounenwd51@gmail.com>
2021-10-02 17:14:26 +0800

    圓形版本

--- draw_crosshair.py
+++ draw_crosshair.py
@@ -4,10 +4,11 @@ import win32api, win32con, win32gui, win32ui
 
 class draw_crosshair():
     def __init__(self):
-        self.fontSize = 48
+        self.fontSize = 36
         self.alpha = 255
-        self.color = win32api.RGB(255,0,0)
-        self.text = '＋'
+        self.color = win32api.RGB(255,255,254)
+        self.color_edge = win32api.RGB(0,0,0)
+        self.text = '．'
         self.DRAWING = False
 
     def join(self):
@@ -60,6 +61,19 @@ class draw_crosshair():
         wndClass.lpszClassName  = className
         # win32gui does not support RegisterClassEx
         self.wndClassAtom = win32gui.RegisterClass(wndClass)
+        
+        self.hInstance_edge = win32api.GetModuleHandle()
+        className = 'MyWindowClassName_edge'
+
+        wndClass_edge                = win32gui.WNDCLASS()
+        # https://msdn.microsoft.com/en-us/library/windows/desktop/ff729176(v=vs.85).aspx
+        wndClass_edge.style          = win32con.CS_HREDRAW | win32con.CS_VREDRAW
+        wndClass_edge.lpfnWndProc    = self.wndProc_edge
+        wndClass_edge.hInstance      = self.hInstance_edge
+        wndClass_edge.hCursor        = win32gui.LoadCursor(None, win32con.IDC_ARROW)
+        wndClass_edge.hbrBackground  = win32gui.GetStockObject(win32con.WHITE_BRUSH)
+        wndClass_edge.lpszClassName  = className
+        self.wndClassAtom_edge = win32gui.RegisterClass(wndClass_edge)
 
     def create_window(self):
         # https://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
@@ -72,13 +86,27 @@ class draw_crosshair():
         style = win32con.WS_DISABLED | win32con.WS_POPUP | win32con.WS_VISIBLE
 
         # https://msdn.microsoft.com/en-us/library/windows/desktop/ms632680(v=vs.85).aspx
+        self.hWindow_edge = win32gui.CreateWindowEx(
+            exStyle,
+            self.wndClassAtom_edge,
+            None, # WindowName
+            style,
+            1   , # x
+            -3, # y
+            win32api.GetSystemMetrics(win32con.SM_CXSCREEN), # width
+            win32api.GetSystemMetrics(win32con.SM_CYSCREEN), # height
+            None, # hWndParent
+            None, # hMenu
+            self.hInstance_edge,
+            None # lpParam
+        )
         self.hWindow = win32gui.CreateWindowEx(
             exStyle,
             self.wndClassAtom,
             None, # WindowName
             style,
-            1, # x
-            -2, # y
+            0, # x
+            -3, # y
             win32api.GetSystemMetrics(win32con.SM_CXSCREEN), # width
             win32api.GetSystemMetrics(win32con.SM_CYSCREEN), # height
             None, # hWndParent
@@ -89,12 +117,15 @@ class draw_crosshair():
 
     def show_window(self):
         # https://msdn.microsoft.com/en-us/library/windows/desktop/ms633540(v=vs.85).aspx
+        win32gui.SetLayeredWindowAttributes(self.hWindow_edge, 0x00ffffff, self.alpha, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
         win32gui.SetLayeredWindowAttributes(self.hWindow, 0x00ffffff, self.alpha, win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
 
         # https://msdn.microsoft.com/en-us/library/windows/desktop/dd145167(v=vs.85).aspx
         #win32gui.UpdateWindow(self.hWindow)
 
         # https://msdn.microsoft.com/en-us/library/windows/desktop/ms633545(v=vs.85).aspx
+        win32gui.SetWindowPos(self.hWindow_edge, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+            win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
         win32gui.SetWindowPos(self.hWindow, win32con.HWND_TOPMOST, 0, 0, 0, 0,
             win32con.SWP_NOACTIVATE | win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_SHOWWINDOW)
         # https://msdn.microsoft.com/en-us/library/windows/desktop/ms633548(v=vs.85).aspx
@@ -120,7 +151,7 @@ class draw_crosshair():
             win32gui.SetTextColor(hdc,self.color)
             # https://msdn.microsoft.com/en-us/library/windows/desktop/dd145037(v=vs.85).aspx
             lf = win32gui.LOGFONT()
-            # lf.lfFaceName = "微軟正黑體"
+            lf.lfFaceName = "微軟正黑體"
             lf.lfHeight = int(round(dpiScale * fontSize))
             lf.lfWeight = 700
             # Use nonantialiased to remove the white edges around the text.
@@ -148,11 +179,47 @@ class draw_crosshair():
         else:
             return win32gui.DefWindowProc(hWnd, message, wParam, lParam)
 
+    def wndProc_edge(self, hWnd, message, wParam, lParam):
+        if message == win32con.WM_PAINT:
+            hdc, paintStruct = win32gui.BeginPaint(hWnd)
+
+            dpiScale = win32ui.GetDeviceCaps(hdc, win32con.LOGPIXELSX) / 60.0
+            fontSize = self.fontSize + 12
+            win32gui.SetTextColor(hdc,self.color_edge)
+            # https://msdn.microsoft.com/en-us/library/windows/desktop/dd145037(v=vs.85).aspx
+            lf = win32gui.LOGFONT()
+            lf.lfFaceName = "微軟正黑體"
+            lf.lfHeight = int(round(dpiScale * fontSize))
+            lf.lfWeight = 700
+            # Use nonantialiased to remove the white edges around the text.
+            lf.lfQuality = win32con.NONANTIALIASED_QUALITY
+            hf = win32gui.CreateFontIndirect(lf)
+            win32gui.SelectObject(hdc, hf)
+
+            rect = win32gui.GetClientRect(hWnd)
+            # https://msdn.microsoft.com/en-us/library/windows/desktop/dd162498(v=vs.85).aspx
+            win32gui.DrawTextW(
+                hdc,
+                self.text,
+                -1,
+                rect,
+                win32con.DT_CENTER | win32con.DT_NOCLIP | win32con.DT_SINGLELINE | win32con.DT_VCENTER
+            )
+            win32gui.EndPaint(hWnd, paintStruct)
+            return 0
+
+        elif message == win32con.WM_DESTROY:
+            # print('Closing the window.')
+            win32gui.PostQuitMessage(0)
+            return 0
+
+        else:
+            return win32gui.DefWindowProc(hWnd, message, wParam, lParam)
 if __name__ == "__main__":
     import time
     d = draw_crosshair()
-    d.draw()
     time.sleep(1)
-    d.modify(fontSize=52, alpha=100, color=(0,255,0))
-    d.redraw()
+    # d.modify(fontSize=52, alpha=255, color=(255,255,254))
+    d.draw()
+    # d.redraw()
     d.join()
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ class main():
     def __init__(self):
         os.system('cls')
         self.d = draw_crosshair()
-        i = input_listener(self.switch)
-        i.start()
+        self.i = input_listener(self.switch)
+        self.i.start()
         self.d.draw()
         self.show = True
         stop = False
@@ -18,30 +18,8 @@ class main():
             print(' -------------------------------')
             print(' > 使用 [左ALT] + [`] 開關準星 <')
             print(' -------------------------------')
-            print(' s [int]          修改準星大小       (預設 48)')
-            print(' a [int]          修改準星透明度0~99 (預設 0)')
-            print(' c [int.int.int]  修改準星RGB顏色    (預設 255.0.0)\n')
-
-            print('指令範例:')
-            print('c 0.255.0\n')
-            modify = input('輸入修改指令:\n')
-            comm = modify.split(' ')[0]
-            if not comm in commed:
-                pass
-            elif len(modify.split(' ')) > 1 and self.show:
-                arg = modify.split(' ')[1]
-                if comm == 's':
-                    self.d.modify(fontSize=int(arg))
-                elif comm == 'a':
-                    a = (100 - int(arg))/100*255
-                    self.d.modify(alpha=int(a))
-                elif comm == 'c':
-                    color = arg.split('.')
-                    if color==['255','255','255']:color=[255,255,254]
-                    self.d.modify(color=color)
-                self.d.close()
-                self.d.redraw()
-            os.system('cls')
+            print('圓形版本不可調整參數')
+            self.i.join()
 
     def switch(self):
         if self.show:
